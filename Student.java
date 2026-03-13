dbb77c61adfb459ac763397e92c07f4b422765db
Parekh Kalp <kalpparekh22@gmail.com>
2026-03-13 18:31:21 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+class Student {
+
+    int id;
+    String name;
+    int marks;
+
+    Student(int id, String name, int marks){
+        this.id=id;
+        this.name=name;
+        this.marks=marks;
+    }
+
+    void display(){
+        System.out.println(id + " " + name + " " + marks);
+    }
+}
